a2e6f1726707794f9eb0789d96bad0c12bf62272
Andrew Donnellan <andrew.donnellan@au1.ibm.com>
2018-09-24 15:06:30 +1000

    print the not yet a opencapibara
    Signed-off-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>

--- hw/npu2-opencapi.c
+++ hw/npu2-opencapi.c
@@ -925,6 +925,15 @@ static int64_t npu2_opencapi_retry_state(struct pci_slot *slot)
 			"Link failed to train, final link status: %016llx, link training status: %016llx\n",
 			 get_odl_status(chip_id, dev->brick_index),
 			 npu2_get_odl_training_status(dev));
+#ifdef DEBUG
+		prlog(PR_ERR, " _______________________________ \n");
+		prlog(PR_ERR, "< It's ODL Training Debug time! >\n");
+		prlog(PR_ERR, " ------------------------------- \n");
+		prlog(PR_ERR, "       \\   ,__,            \n");
+		prlog(PR_ERR, "        \\  (oo)____        \n");
+		prlog(PR_ERR, "           (__)    )\\      \n");
+		prlog(PR_ERR, "              ||--|| *     \n");
+#endif
 		return OPAL_HARDWARE;
 	}
 
